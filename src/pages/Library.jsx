83128b90dd0b36ca6c88af80069e42a30c7e7a6b
ifandika <ifandika014@gmail.com>
2025-12-30 11:03:14 +0700

    update library

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -26,7 +26,79 @@ export default function Library() {
           {/* COMPUTER SCIENCE */}
           <Typography variant="h6"><i>1. Computer Science</i></Typography>
           <ul>
-            <li><a href=""></a></li>
+            {/* DSA (Data Structure and Algorithms) */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>DSA (Data Structure and Algorithms) - Basics</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>DSA (Data Structure and Algorithms) - Asymptotic Notation</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>DSA (Data Structure and Algorithms) - Books, Magazine, Video, etc.</i></a>
+            </li>
+            
+            <br />
+
+            {/* System Design */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>System Design - UML (Unified Modeling Language)</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>System Design - Books, Magazine, Video, etc.</i></a>
+            </li>
+
+            <br />
+
+            {/* Design Pattern */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Design Pattern - Basics</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Design Pattern - Books, Magazine, Video, etc.</i></a>
+            </li>
+
+            <br />
+
+            {/* Database */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Database/DBMS - Basics</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Database/DBMS - Books, Magazine, Video, etc.</i></a>
+            </li>
+
+            <br />
+
+            {/* Web Technology */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Web Technology - Basics</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>Web Technology - Books, Magazine, Video, etc.</i></a>
+            </li>
+
+            <br />
+
+            {/* API (Application Programming Interface) */}
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>API (Application Programming Interface) - Basics</i></a>
+            </li>
+            <li><a
+              href="https://docs.google.com/document/d/1Dg_UEbevQCmsXydzccVvlxpZXkkmLjD2BXxncpj5hHs/edit?usp=sharing">
+              <i>API (Application Programming Interface) - Books, Magazine, Video, etc.</i></a>
+            </li>
           </ul>
 
           {/* PYTHON PROGRAMMING LANGUAGE */}
@@ -51,6 +123,38 @@ export default function Library() {
             <li><a
               href="https://docs.google.com/document/d/1rmpfANIikvl8pVXDhdMXRBkfIZtFads-t3GeTjQo35Y/edit?usp=sharing">
               Tutorial Git Bash</a></li>
+          </ul>
+
+          {/* Graphic Design */}
+          <Typography variant="h6"><i>4. Graphic Design</i></Typography>
+          <ul>
+            <li><a
+              href="https://docs.google.com/document/d/1JZ1qPHul7Gz1Byftann7_xralYDtk9qdQuubQKuo5wI/edit?usp=sharing">
+              Tutorial Desain Grafis</a></li>
+          </ul>
+          
+          {/* OPERATING SYSTEM */}
+          <Typography variant="h6"><i>5. Operating Systems</i></Typography>
+          <ul>
+            <li><a
+              href="https://docs.google.com/document/d/1U0nx3Wwed_DiWe5mHQSM002UQTdY9G0nJ9oPU9vyzIk/edit?usp=sharing">
+              Tutorial Sistem Operasi Dasar</a></li>
+          </ul>
+
+          {/* ELEKTRONICS */}
+          <Typography variant="h6"><i>5. Electronic</i></Typography>
+          <ul>
+            <li><a
+              href="https://docs.google.com/document/d/1sFVDkWI6mUpwqvpid063zxIl0LdTTYqwc1ORVUF4FfI/edit?usp=sharing">
+              Tutorial Elektronika Dasar</a></li>
+          </ul>
+
+          {/* ARDUINO */}
+          <Typography variant="h6"><i>6. Arduino</i></Typography>
+          <ul>
+            <li><a
+              href="https://docs.google.com/document/d/1U0nx3Wwed_DiWe5mHQSM002UQTdY9G0nJ9oPU9vyzIk/edit?usp=sharing">
+              Tutorial Arduino Dasar</a></li>
           </ul>
 
         </Container>
